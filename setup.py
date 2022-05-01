b38b8cbd5dbde8f05175dd754f8cac3cf31a830f
oliveirajo <joel.amarofo@gmail.com>
2022-05-01 12:17:17 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proposition of the btime slot too go out with all the participants',
+    author='oliveirajo',
+    license='MIT',
+)
